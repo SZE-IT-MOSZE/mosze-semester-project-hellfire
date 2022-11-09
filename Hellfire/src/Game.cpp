0b71609e61a356ff1a3d3f15a979bcbcc7d4c82b
BotiD3 <nagybotond123@gmail.com>
2022-11-09 22:26:08 +0100

    Game_cpp_method_declaration

---
+++ Hellfire/src/Game.cpp
@@ -0,0 +1,49 @@
+#include "Game.h"
+
+using namespace std;
+
+Game::Game(){
+
+}
+
+static Chapter& Game::getChapter(){
+
+}
+
+static Chapter Game::loadNextChapter(){
+
+}
+
+static Player& Game::getPlayer(){
+
+}
+
+static void Game::loadGame(string name){
+
+}
+
+static void Game::saveHame(string name){
+
+}
+
+static void Game::openInventory(){
+
+}
+
+static void Game::openAttribueWindow(){
+
+}
+
+static void Game::openMenu{
+
+}
+
+static void Game::newGame(){
+
+}
+
+static void Game::turn(){
+
+}
+
+
