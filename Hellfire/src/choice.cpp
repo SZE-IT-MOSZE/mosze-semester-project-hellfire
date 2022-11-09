f9cc6e9fa0bb5653278b616805c30e016ddc2308
BotiD3 <nagybotond123@gmail.com>
2022-11-10 00:27:28 +0100

    choice.cpp method declaration

---
+++ Hellfire/src/choice.cpp
@@ -0,0 +1,31 @@
+#include "choice.h"
+
+using namespace std;
+
+string Choice::getText(){
+
+}
+
+int Choice::getType() {
+
+}
+
+bool Choice::isFailed(){
+
+}
+
+bool Choice::isChosen(){
+
+}
+
+int Choice::getDifficulty(){
+
+}
+
+int Choice::getExperience(){
+
+}
+
+int Choice::getCorruption(){
+
+}
