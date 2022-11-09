e5d1c63e274b143a99ce179124c0ba39b356ac36
BotiD3 <nagybotond123@gmail.com>
2022-11-09 23:41:15 +0100

    inventory.cpp method declaration

---
+++ Hellfire/src/inventory.cpp
@@ -0,0 +1,18 @@
+#include "inventory.h"
+
+
+void Inventory::addItem(Item item) {
+
+}
+
+void Inventory::deleteItem(string name){
+
+}
+
+vector<Item>& Inventory::getItems(){
+
+}
+
+Item& Inventory::selectedItem(string name) {
+
+}
