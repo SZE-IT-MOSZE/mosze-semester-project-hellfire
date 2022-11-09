1f98ae038f539e459e102529dd052c54efaec276
BotiD3 <nagybotond123@gmail.com>
2022-11-09 23:10:33 +0100

    player_cpp_method_declaration

---
+++ Hellfire/src/player.cpp
@@ -0,0 +1,28 @@
+#include "player.h"
+
+Inventory& Player::getInventory(){
+
+}
+Attributes& Player::getAttributes() {
+
+}
+
+string Player::getName(){
+
+}
+
+void Player::setSkillPoints(int n){
+
+}
+
+int Player::getSkillPoints(){
+
+}
+
+void Player::setName(string name) {
+
+}
+
+void Player::setExperience(int amount) {
+
+}
