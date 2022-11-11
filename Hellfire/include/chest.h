bc1618c1f9c3bbada1fa12e6b128227679c656bc
BotiD3 <nagybotond123@gmail.com>
2022-11-11 18:17:40 +0100

    chest header declaration

---
+++ Hellfire/include/chest.h
@@ -0,0 +1,16 @@
+#ifndef CHEST_H
+#define CHEST_H
+
+#include "item.h"
+
+class Chest
+{
+    public:
+        Item Open();
+    protected:
+
+    private:
+        Item item;
+};
+
+#endif // CHEST_H
