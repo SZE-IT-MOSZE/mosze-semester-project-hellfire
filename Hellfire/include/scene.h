8b1d23b9119546e98bd4c6734643b6c2eb57b91b
Syadon <bokagabor4@gmail.com>
2022-11-12 19:46:25 +0100

    constructor and some additional getters defined

--- Hellfire/include/scene.h
+++ Hellfire/include/scene.h
@@ -9,12 +9,20 @@ class Scene
 {
     public:
         void chooseChoice();
+        Scene(std::string sb, std::string a,  std::string order, std::vector<Choice> chs);
+        std::string getArt();
+        std::string getStorybit();
+        std::string getOrder();
+        std::vector<Choice>& getChoices();
+        std::vector<Choice> choices;
 
     protected:
 
     private:
         std::string art;
-        std::vector<Choice> choices;
+        std::string order;
+        std::string storybit;
+
 };
 
 #endif // SCENE_H
--- Hellfire/src/scene.cpp
+++ Hellfire/src/scene.cpp
@@ -1,5 +1,28 @@
 #include "scene.h"
 
+
+Scene::Scene(std::string sb, std::string a,  std::string odr, std::vector<Choice> chs): choices(chs) {
+    storybit = sb;
+    art = a;
+    order = odr;
+}
+
 void Scene::chooseChoice() {
 
+}
+
+std::string Scene::getArt() {
+    return art;
+}
+
+std::string Scene::getStorybit() {
+    return storybit;
+}
+
+std::string Scene::getOrder() {
+    return order;
+}
+
+std::vector<Choice>& Scene::getChoices() {
+    return choices;
 }
