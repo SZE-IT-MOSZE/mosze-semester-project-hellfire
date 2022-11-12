059f92b817d20b9b2bf2888d8396eca39800ff3b
Syadon <bokagabor4@gmail.com>
2022-11-12 19:38:38 +0100

    enums moved to different header

---
+++ Hellfire/include/enums.h
@@ -0,0 +1,24 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+enum class ChoiceType
+{
+    Strength,
+    Intelligence,
+    Persuasion,
+    Default
+};
+
+enum class ConsumableType
+{
+    Strength,
+    Intelligence,
+    Persuasion
+};
+
+enum class WeaponType
+{
+    Staff, Sword
+};
+
+#endif
