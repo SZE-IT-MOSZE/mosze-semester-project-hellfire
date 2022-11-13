8895ad874e49c0237d83e79fe01d8317265e529c
Syadon <bokagabor4@gmail.com>
2022-11-13 12:08:03 +0100

    refactored openMenu with the new Render class' methods

--- Hellfire/src/game.cpp
+++ Hellfire/src/game.cpp
@@ -3,10 +3,12 @@
 #include <vector>
 #include "choice.h"
 #include "scene.h"
+#include "render.h"
 
 using namespace tinyxml2;
 
 const char* Game::STORY_PATH = "story/story.xml";
+const char* Game::GAME_TITLE = "Fantasy Game";
 
 std::string init = "";
 std::vector<Scene> initScns = {};
@@ -14,9 +16,7 @@ Chapter Game::actualChapter = Chapter(-1, init, initScns);
 
 
 
-Game::Game(){
-
-}
+Game::Game(){ }
 
 Chapter& Game::getChapter(){
     return actualChapter;
@@ -46,8 +46,90 @@ void Game::openAttribueWindow(){
 
 }
 
-void Game::openMenu() {
+int Game::openMenu(SDL_Surface* screen, TTF_Font* font, TTF_Font* titleFont) {
+    Uint32 time;
 
+    const int NUMMENU=3;
+    const char* labels[NUMMENU] = {"Új játék", "Játék betöltése", "Kilépés"};
+    SDL_Surface* menus[NUMMENU];
+    SDL_Texture* textures[NUMMENU];
+    bool selected[NUMMENU] = {0,0,0};
+    SDL_Color color[2] = {{255,255,255}, {255,0,0}};
+
+    SDL_SetRenderDrawColor( Render::renderer, 0, 0, 0, 255 );
+
+    SDL_Surface* title = TTF_RenderUTF8_Solid(titleFont, GAME_TITLE, color[0]);
+
+    menus[0] = TTF_RenderUTF8_Solid(font, labels[0], color[0]);
+    menus[1] = TTF_RenderUTF8_Solid(font, labels[1], color[0]);
+    menus[2] = TTF_RenderUTF8_Solid(font, labels[2], color[0]);
+    SDL_Rect pos[NUMMENU] = { {Render::WIDTH / 2 - menus[0]->clip_rect.w/2,
+                              Render::HEIGHT / 2 - menus[0]->clip_rect.h,
+                              menus[0]->w, menus[0]->h},
+                              {Render::WIDTH / 2 - menus[1]->clip_rect.w/2,
+                               Render::HEIGHT / 2 + menus[1]->clip_rect.h,
+                               menus[1]->w, menus[1]->h},
+                               {Render::WIDTH / 2 - menus[2]->clip_rect.w/2,
+                               Render::HEIGHT / 2 + (menus[2]->clip_rect.h * 3),
+                               menus[2]->w, menus[2]->h}
+                            };
+
+     SDL_Rect titlePos =  {Render::WIDTH / 2 - title->clip_rect.w / 2,
+                                   Render::HEIGHT / 4 - title->clip_rect.h,
+                                   title->w, title->h};
+
+     Render::renderSurface(title, titlePos);
+     Render::renderSurface(menus[0], pos[0]);
+     Render::renderSurface(menus[1], pos[1]);
+     Render::renderSurface(menus[2], pos[2]);
+
+    //a menü event ciklusa
+    SDL_Event event;
+    while(true) {
+        time=SDL_GetTicks();
+        if(SDL_PollEvent(&event)) {
+            switch(event.type) {
+                case SDL_QUIT:
+                    return -1;
+                case SDL_KEYDOWN:
+                    if(SDLK_0 == event.key.keysym.sym) {
+                        //ha 0-át nyomunk és nincs kiválasztva:
+                        if(!selected[0])
+                        {
+                            selected[0] = true;
+
+                            if(selected[1]) {
+                                selected[1] = false;
+                                menus[1] = TTF_RenderUTF8_Solid(font, labels[1], color[0]);
+                                Render::renderSurface(menus[1], pos[1]);
+                            }
+                            else if(selected[2]) {
+                                selected[2] = false;
+                                menus[2] = TTF_RenderUTF8_Solid(font, labels[2], color[0]);
+                                Render::renderSurface(menus[2], pos[2]);
+                            }
+                            menus[0] = TTF_RenderUTF8_Solid(font, labels[0], color[1]);
+                            Render::renderSurface(menus[0], pos[0]);
+                        }
+                    }
+
+                    //ha valami ki van választva akkor enter lenyomásával visszaadjuk melyik
+                     else if(SDL_SCANCODE_RETURN == event.key.keysym.scancode) {
+                        for(int i = 0; i < NUMMENU; i++) {
+                            if(selected[i] == true) {
+                                return i+1;
+                            }
+                        }
+                    }
+
+                     //escape-el kilépünk a menübõl
+                    else if(SDLK_ESCAPE == event.key.keysym.sym) {
+                        std::cout << "Escape" << std::endl;
+                        return 0;
+                    }
+            }
+        }
+    }
 }
 
 void Game::newGame(){
