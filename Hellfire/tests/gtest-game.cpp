fe3716519828ecfbf8d1766fcfc52315e6cde921
BotiD3 <nagybotond123@gmail.com>
2022-11-18 23:32:03 +0100

    gtest-game update (undefined reference to sdl)

--- Hellfire/tests/gtest-game.cpp
+++ Hellfire/tests/gtest-game.cpp
@@ -1,6 +1,24 @@
 #include <gtest/gtest.h>
+//#include "game.h"
 
-TEST(GameTest, SampleTest)
+#include <string>
+
+
+class GameTest : public ::testing::Test{
+    protected:
+    void SetUp() override {
+        //Game::newGame();
+
+    }
+
+
+};
+
+TEST_F(GameTest, SampleTest)
 {
     EXPECT_EQ(1,1);
+    EXPECT_NE(1,2);
 }
+
+
+
