5c116b32f51e353b802a7edc20006dead708c578
Syadon <bokagabor4@gmail.com>
2022-12-11 23:51:59 +0100

    added default constructor with initialization

--- Hellfire/src/attributes.cpp
+++ Hellfire/src/attributes.cpp
@@ -2,24 +2,37 @@
 
 using namespace std;
 
+Attributes::Attributes() {
+    strength = 0;
+    intelligence = 0;
+    persuasion = 0;
+    corruptionLevel = 0;
+}
+
 int Attributes::getStrength() {
+    return strength;
 }
 
 int Attributes::getIntelligence(){
-
+    return intelligence;
 }
 
 int Attributes::getPersuasion(){
-
+    return persuasion;
 }
 
 void Attributes::upgradeStrength(){
-
+     strength += 1;
 }
 
 void Attributes::upgradeIntelligence(){
+    intelligence += 1;
 }
 
 void Attributes::upgradePersuasion() {
+    persuasion += 1;
+}
 
+void Attributes::setCorruption(int amount) {
+    corruptionLevel += amount;
 }
